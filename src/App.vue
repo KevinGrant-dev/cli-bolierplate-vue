d74bcdf87bb5305c3af7d2bbbcff59fe93f160e8
Enthusiast301 <enthusiast301@gmail.com>
2022-06-09 09:41:33 -0400

    upload to github

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/images/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import { HelloWorld } from 'components';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './styles/main.scss';
+
+#app {
+  margin-top: 60px;
+  color: $navy;
+  text-align: center;
+}
+
+img {
+  width: auto;
+}
+</style>
